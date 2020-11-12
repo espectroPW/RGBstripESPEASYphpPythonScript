a15f54b0bea49177cc9baa09117849c77de02726
Espectro <72676549+espectroPW@users.noreply.github.com>
2020-11-12 17:19:58 +0100

    Update index.php added local database
    Added local database to store rgb value and switch state.

--- rgb.php
+++ rgb.php
@@ -6,6 +6,10 @@
 
 session_start();
 
+
+$sw1 = $_SESSION['switch1'];
+$sw = $_SESSION['switch'];
+
 if(isset($_GET['red'])){
     $red = $_GET['red'];
 	$_SESSION['czerwony'] = $red;
@@ -13,6 +17,46 @@ if(isset($_GET['red'])){
 	$_SESSION['zielony'] = $green;
     $blue = $_GET['blue'];
 	$_SESSION['niebieski'] = $blue;
+	
+
+
+
+	
+	
+	
+	
+	
+	
+	# zapis do pliku z sesji
+	$fp = fopen("database.txt", "w");
+
+	#fwrite($fp, "czerwony=");
+	fwrite($fp, $red);
+	fwrite($fp, "\n");
+
+
+	#fwrite($fp, "zielony=");
+	fwrite($fp, $green);
+	fwrite($fp, "\n");
+
+
+	#fwrite($fp, "niebieski=");
+	fwrite($fp, $blue);
+	fwrite($fp, "\n");
+
+	#fwrite($fp, "switch1=");
+	fwrite($fp, $sw1);
+	fwrite($fp, "\n");
+
+	#fwrite($fp, "switch=");
+	fwrite($fp, $sw);
+	fwrite($fp, "\n");
+
+	fclose($fp);
+
+
+	
+	
     
     //trigger rgb.py python code with the red, green, and blue params
     $output = exec('python3 rgb.py ' . $red . ' ' . $green . ' ' . $blue . ' 2>&1');
